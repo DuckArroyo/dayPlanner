2f25c5a040fd0a2720da71dc8fa52f33651097a6
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-05 21:08:22 -0700

    testing saving and loading

--- javaScript.js
+++ javaScript.js
@@ -5,15 +5,21 @@
 
 const StartDate = $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-//Storage
-var saveToDo = function () {
-  localStorage.setItem("toDo", JSON.stringify(tasks));
-};
-
-var loadToDo = function () {
-  toDos = JSON.parse(localStorage.getItem("toDo"));
-  // if nothing in localStorage, create a new object to track all task status arrays
-  if (!toDo) {
-    toDo = [];
-  }
-};
+//jquery sandwich example
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    console.log("btn clicked");
+    var userText = $(this).siblings(".description").val();
+    console.log(userText);
+    var hour = $(this).parent().attr("id");
+    console.log(hour);
+    localStorage.setItem(hour, userText);
+    // const toDos = {
+    //   text: userText,
+    //   time: hour
+    // }
+    // localStorage.setItem(toDos);
+  });
+  $("#priority .description").val(localStorage.getItem("#priority"));
+  $("#9am .description").val(localStorage.getItem("#9am"));
+});
